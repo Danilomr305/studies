e85cfe624ce8fbb43a8457f667c43a7f09c1b150
Danilomr305 <128266870+Danilomr305@users.noreply.github.com>
2024-03-07 21:21:51 -0300

    animação de tela completa

--- animation_tela/lib/main.dart
+++ animation_tela/lib/main.dart
@@ -1,44 +1,60 @@
 // ignore_for_file: unused_import
-
-import 'package:animation_tela/page_dois.dart';
 import 'package:flutter/material.dart';
+import './page_dois.dart';
 import 'package:page_transition/page_transition.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-        title: const Text('PAGE HOME'),
+      home: Navigator(
+        onGenerateRoute: (settings) {
+          return MaterialPageRoute( builder: (context) =>const HomePage(),
+          );
+        },
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: ElevatedButton(
-                onPressed: () => Navigator.push(
-                    context,
-                    PageTransition(
-                      child: const PageAnimacao(), 
-                      type: PageTransitionType.fade)
-                  )
-                , 
-                child: const Text('Ir para tela 2')
-              ),
-            )
-          ],
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black54,
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent.shade100,
+        title:const Text('Page Home',
+        style: 
+          TextStyle(
+            color: Colors.black45,
+            fontWeight: FontWeight.bold,
+            fontFamily: AutofillHints.username,
+          ),
         ),
       ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => Navigator.push(
+            context,
+            PageTransition(
+              child: const PageAnimacao(),
+              type: PageTransitionType.rotate,
+              alignment: Alignment.center,
+              duration: const Duration(milliseconds: 600)
+            ),
+          ),
+          child: const Text('Ir para pagina 2'),
+        ),
       ),
     );
   }
