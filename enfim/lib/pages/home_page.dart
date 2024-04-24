0439f27e74e50d313a1afa3e54bb328d1bcf8abb
Danilomr305 <128266870+Danilomr305@users.noreply.github.com>
2024-04-24 14:12:39 -0300

    .

--- enfim/lib/pages/home_page.dart
+++ enfim/lib/pages/home_page.dart
@@ -1,43 +1,18 @@
-// ignore_for_file: unused_import, sized_box_for_whitespace
-
-import 'package:enfim/models/fotos_models.dart';
-import 'package:enfim/repository/fotos_repository.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
   
-  get ima => null;
 
   @override
   Widget build(BuildContext context) {
-
-    final images = ImagemRepository.images;
-
-    return SingleChildScrollView(
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Eu tenho que conseguir'
-          ),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                height: 400,
-                
-                child: ListTile(
-                  leading: Image.network(images[ima].title),
-                  title: Text(images[ima].name),
-                  trailing: Text(images[ima].price.toString()),
-                )
-              )
-            ]
-          ),
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
